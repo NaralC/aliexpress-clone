a31ff923138f5dc3159cc1625099152e317382c1
Naral <77269201+NaralC@users.noreply.github.com>
2023-08-10 00:14:07 +0100

    navbar

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+import MainLayout from "~/layouts/main-layout.vue";
+</script>
+
+<template>
+  <div class="bg-red-500">
+    <MainLayout d="IndexPage" class="mt-4 max-w-[1200px] mx-auto px-2"
+      >Index Page</MainLayout
+    >
+  </div>
+</template>
